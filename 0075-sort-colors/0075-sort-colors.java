88400c1df86424dc2d5598f14c08408f5a87cacc
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2025-04-18 06:11:17 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.45%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,9 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int lo, mid, hi, temp;
-        lo = 0;
-        mid = 0;
-        hi = nums.length-1;
+        int lo = 0, mid = 0, temp;
+        int hi = nums.length-1;
         while(mid<=hi){
             switch(nums[mid]){
                 case 0: {
@@ -24,8 +22,7 @@ class Solution {
                     hi--;
                     break;
                     }
-            }
-        }
-
+            }  
+        }      
     }
 }
