78eff70a06e8956c4e0bfd0621d87308c14426fe
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2025-06-10 11:17:50 +0530

    Time: 30 ms (69.51%), Space: 51.5 MB (71.45%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,10 +5,7 @@ class Solution {
         Arrays.sort(arr);
 
         for (int i = 0; i < n; i++) {
-            //remove duplicates:
             if (i != 0 && arr[i] == arr[i - 1]) continue;
-
-            //moving 2 pointers:
             int j = i + 1;
             int k = n - 1;
             while (j < k) {
@@ -22,7 +19,6 @@ class Solution {
                     ans.add(temp);
                     j++;
                     k--;
-                    //skip the duplicates:
                     while (j < k && arr[j] == arr[j - 1]) j++;
                     while (j < k && arr[k] == arr[k + 1]) k--;
                 }
