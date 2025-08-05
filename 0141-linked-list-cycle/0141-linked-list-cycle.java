b30ad8456a73630359620dabc14fedafd1012982
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2025-08-05 09:14:54 +0530

    Time: 0 ms (100%), Space: 44.6 MB (38.26%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,30 +9,27 @@
  *     }
  * }
  */
-//  class ListNode{
-//     public int data;
-//     public ListNode next;
-//     public ListNode(int data, ListNode next){
-//         this.data = data;
-//         this.next = next;
-//     }
-//     public ListNode(int data){
-//         this.data = data;
-//         this.next = null;
-//     }
-//  }
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode temp = head;
-        Map<ListNode,Integer> np = new HashMap<>();
-        while(temp != null){
-            if(np.containsKey(temp))
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast= fast.next.next;
+            if(fast == slow)
             return true;
-
-            np.put(temp,1);
-            temp = temp.next;
         }
-
         return false;
+        // ListNode temp = head;
+        // Map<ListNode,Integer> np = new HashMap<>();
+        // while(temp != null){
+        //     if(np.containsKey(temp))
+        //     return true;
+
+        //     np.put(temp,1);
+        //     temp = temp.next;
+        // }
+
+        // return false;
     }
 }
