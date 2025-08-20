33372d0c554baf786290454a3e6881c9088e90bb
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2025-08-20 17:49:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,27 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
+    public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        int n = arr.length;
-        Arrays.sort(arr);
-
-        for (int i = 0; i < n; i++) {
-            if (i != 0 && arr[i] == arr[i - 1]) continue;
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = arr[i] + arr[j] + arr[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int p1 = i+1, p2 = nums.length-1;
+            while(p1<p2){
+                int total = nums[p1]+nums[p2]+nums[i];
+                List<Integer> temp = new ArrayList<>();
+                if(total == 0){
+                    temp.add(nums[i]);
+                    temp.add(nums[p1]);
+                    temp.add(nums[p2]);
                     ans.add(temp);
-                    j++;
-                    k--;
-                    while (j < k && arr[j] == arr[j - 1]) j++;
-                    while (j < k && arr[k] == arr[k + 1]) k--;
+                    p1++;
+                    p2--;
+                    while(p1<p2 && nums[p1] == nums[p1-1]) continue;
+                    while(p1<p2 && nums[p2] == nums[p2-1]) continue;
                 }
+                else if(total<0)
+                p1++;
+                else
+                p2--;
             }
         }
         return ans;
